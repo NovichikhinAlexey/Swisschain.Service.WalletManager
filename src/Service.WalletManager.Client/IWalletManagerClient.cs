7d9a78417e35fda848dc94627502a3edaedd70fe
Aleksandr Belousov <alexandr.belousov@lykke.com>
2020-02-24 16:54:00 +0400

    Updated client interface

--- src/Service.WalletManager.Client/IWalletManagerClient.cs
+++ src/Service.WalletManager.Client/IWalletManagerClient.cs
@@ -5,5 +5,11 @@ namespace Service.WalletManager.Client
     public interface IWalletManagerClient
     {
         Monitoring.MonitoringClient Monitoring { get; }
+
+        Wallets.WalletsClient Wallets { get; }
+
+        Operations.OperationsClient Operations { get; }
+
+        Balances.BalancesClient Balances { get; }
     }
 }
